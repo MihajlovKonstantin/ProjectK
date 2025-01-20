97577108e6378c3efb7194466315b8e528c3196b
kd1387707 <kd1387707@st.kobedenshi.ac.jp>
2025-01-20 14:03:55 +0900

    gomen

--- Src/Application/BlockEnum.h
+++ Src/Application/BlockEnum.h
@@ -1,4 +1,4 @@
 #pragma once
 enum IceBlockSelect { COUNTIBS = 2, Surface = 0, Inside };
-enum BlockEditerSelect { COUNTBES = 5, Ground = 1, Ice, IceWater, Ladder };
+enum BlockEditerSelect { COUNTBES = 6, Ground = 1, Ice, IceWater, Ladder,Lava };
 //enum TypeBlock { COUNTTB = 3, Base = 0, Snow, Ice };
